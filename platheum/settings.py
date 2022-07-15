64fb8f45ece25e71c747bfda8da0f12d5047c7c0
platheum <platheum@gmail.com>
2022-07-15 04:16:13 +0100

    third commit

--- platheum/settings.py
+++ platheum/settings.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 import environ
+
 env = environ.Env()
 
 environ.Env.read_env()
